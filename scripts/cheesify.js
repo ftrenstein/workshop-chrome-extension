e85560fe3769a500a8fbffa6364162b0a1f61651
renstein <pilatera@mail.ru>
2024-09-02 12:08:49 +0200

    finish

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,15 @@
+
+
 function cheesify() {
-  // TODO: Add the image replacement script here
+  document.querySelectorAll('img').forEach( (img) => {
+    img.src = 'https://img.freepik.com/free-photo/delicious-pieces-cheese_144627-43352.jpg?t=st=1724614215~exp=1724617815~hmac=8135027ab18782c8d32f9d712d9c47c1742c89b8385ddf1fc8b51b36d727de6e&w=826';
+    img.srcset = img.src;
+  })
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === 'cheesify') cheesify();
+  }
+);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,11 @@
 function sendCheesifyMsg() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { // Finds tabs that are active in the current window
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'cheesify' }); // Sends a message (object) to the first tab (tabs[0])
+  });
   // TODO: Write a function to send a message to the active tab to 'cheesify' it
 }
 
 // TODO: Add an event listener to trigger the function above when clicking the 'Cheesify' button
+
+const button = document.getElementById('cheesify');
+button.addEventListener('click', sendCheesifyMsg);
